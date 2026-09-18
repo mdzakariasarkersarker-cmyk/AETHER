d40f58a31475bda55339b15b70bda92b30108f97
mdzakariasarkersarker-cmyk <mdzakariasarkersarker@gmail.com>
2026-09-18 20:43:11 +0600

    Create SQLite data directory automatically

--- database.js
+++ database.js
@@ -1,5 +1,8 @@
 const Database = require("better-sqlite3");
 const path = require("path");
+const fs = require("fs");
+const dataDir = path.join(__dirname, "data");
+fs.mkdirSync(dataDir, { recursive: true });
 
 const db = new Database(path.join(__dirname, "data", "aqlyven.db"));
 
