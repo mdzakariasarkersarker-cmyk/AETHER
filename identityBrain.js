ebe0b9fefce8b209ef1b9ba1edb130ee6c3cb1fc
mdzakariasarkersarker-cmyk <mdzakariasarkersarker@gmail.com>
2026-09-26 22:20:17 +0600

    Update AQLYVEN help line

--- identityBrain.js
+++ identityBrain.js
@@ -144,7 +144,7 @@ function identityReply(question) {
     q.includes("হেল্প") ||
     q.includes("support")
   ) {
-    return "🆘 AQLYVEN Help Line: 01861999585";
+    return "🆘 AQLYVEN Help Line: +8809658740781";
   }
 
   return null;
